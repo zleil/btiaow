3b28c20d4bf04ee3beb5093df8c2c739f797396f
zleil <cfb0d4b6.le@gmail.com>
2013-10-11 01:45:03 +0800

    a

---
+++ btiao.user/WEB-INF/src/com/btiao/user/restlet/ResBTiaoAllUser.java
@@ -0,0 +1,39 @@
+package com.btiao.user.restlet;
+
+import java.util.Collection;
+
+import org.restlet.data.Form;
+
+import com.btiao.base.exp.BTiaoExp;
+import com.btiao.base.oif.restlet.ResBTBase;
+import com.btiao.user.domain.BTiaoUser;
+import com.btiao.user.service.UserMgr;
+
+public class ResBTiaoAllUser extends ResBTBase {
+
+	@Override
+	protected Object get(Form form) throws BTiaoExp {
+		Collection<BTiaoUser> us = UserMgr.instance().getAllUser();
+		return us;
+	}
+
+	@Override
+	protected Object put(Object arg) throws BTiaoExp {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Object post(Object arg, Collection<String> attrList)
+			throws BTiaoExp {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Object del(Object arg) throws BTiaoExp {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
