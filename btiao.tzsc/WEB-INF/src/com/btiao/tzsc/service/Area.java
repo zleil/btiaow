086f7ab1dd8d4316f3dadf0ea1ac7377455a950f
zleil <cfb0d4b6.le@gmail.com>
2014-08-10 15:24:57 +0800

    a

---
+++ btiao.tzsc/WEB-INF/src/com/btiao/tzsc/service/Area.java
@@ -0,0 +1,10 @@
+package com.btiao.tzsc.service;
+
+public class Area {
+	public long id;
+	public String desc;
+	
+	public String toString() {
+		return "{id:"+id+",desc:\""+desc+"\"}";
+	}
+}
---
+++ btiao.tzsc/WEB-INF/src/com/btiao/tzsc/service/AreaMgr.java
@@ -0,0 +1,75 @@
+package com.btiao.tzsc.service;
+
+import java.util.HashMap;
+
+public class AreaMgr {
+	static public synchronized AreaMgr instance() {
+		if (inst == null) {
+			inst = new AreaMgr();
+		}
+		return inst;
+	}
+	
+	static public String persistFn = "areas";
+	static private AreaMgr inst = null;
+	
+	public synchronized void addArea(Area area) {
+		all.put(area.id, area);
+		this.changed = true;
+	}
+	
+	public synchronized Area getArea(long id) {
+		return this.all.get(id);
+	}
+	
+	private AreaMgr() {
+		load();
+		
+		if (!all.containsKey(65537)) {
+			Area area = new Area();
+			area.id = 65537;
+			area.desc = "望春园";
+			addArea(area);
+		}
+		
+		PersistObj.addBackTask(new Runnable() {
+
+			@Override
+			public void run() {
+				synchronized (AreaMgr.instance()) {
+					if (!AreaMgr.instance().hasChanged()) {
+						return;
+					}
+					
+					new PersistObj().persist(persistFn, all);
+					
+					AreaMgr.instance().setUnchanged();
+				}
+			}
+			
+		});
+	}
+	
+	@SuppressWarnings("unchecked")
+	private void load() {
+		Object persistAll = new PersistObj().load(AreaMgr.persistFn);
+		if (persistAll == null) {
+			MyLogger.get().warn("get persist areamgr failed!");
+			return;
+		}
+		
+		all = (HashMap<Long, Area>) persistAll;
+	}
+	
+	private void setUnchanged() {
+		changed = false;
+	}
+	
+	private boolean hasChanged() {
+		return changed;
+	}
+	
+	private HashMap<Long,Area> all = new HashMap<Long,Area>();
+	
+	private volatile boolean changed = false;
+}
